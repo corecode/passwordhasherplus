5307a0e54c3814bdcbe46220c2a872a95d6088da
Simon Schubert <2@0x2c.org>
2014-04-29 12:43:45 +0200

    untabify

--- data/lib/common.js
+++ data/lib/common.js
@@ -35,63 +35,61 @@
 
 var debug = false;
 
-var Set = function () {}
-Set.prototype.add = function (o) { this[o] = true; }
-Set.prototype.remove = function (o) { delete this[o]; }
+var Set = function() {}
+Set.prototype.add = function(o) { this[o] = true; };
+Set.prototype.remove = function(o) { delete this[o]; };
 
-function toSet (array) {
-	var s = new Set ();
-	for (var i = 0; i < array.length; ++i) {
-		s.add (array[i]);
-	}
-	return s;
+function toSet(array) {
+    var s = new Set();
+    for(var i = 0; i < array.length; ++i) {
+	s.add(array[i]);
+    }
+    return s;
 }
 
-// XXX this function is only used by content-script.js and the portable page
-// should pull out the rest into a separate file.
-function generateHash (config, input) {
-	var tag = config.tag;
+function generateHash(config, input) {
+    var tag = config.tag;
 
-	if (false == config.options.compatibilityMode && null != config.policy.seed) {
-		tag = PassHashCommon.generateHashWord (
-			config.policy.seed,
-			tag,
-			24,
-			true, // require digits
-			true, // require punctuation
-			true, // require mixed case
-			false, // no special characters
-			false // only digits
-		);
-	}
-
-	return PassHashCommon.generateHashWord (
-		tag,
-		input,
-		config.policy.length,
-		true, // require digits
-		config.policy.strength > 1, // require punctuation
-		true, // require mixed case
-		config.policy.strength < 2, // no special characters
-		config.policy.strength == 0 // only digits
+    if (!config.options.compatibilityMode && config.policy.seed != null) {
+	tag = PassHashCommon.generateHashWord(
+	    config.policy.seed,
+	    tag,
+	    24,
+	    true, // require digits
+	    true, // require punctuation
+	    true, // require mixed case
+	    false, // no special characters
+	    false // only digits
 	);
+    }
+
+    return PassHashCommon.generateHashWord(
+	tag,
+	input,
+	config.policy.length,
+	true, // require digits
+	config.policy.strength > 1, // require punctuation
+	true, // require mixed case
+	config.policy.strength < 2, // no special characters
+	config.policy.strength == 0 // only digits
+    );
 }
 
-function bump (tag) {
-	var re = new RegExp ("^([^:]+?)(:([0-9]+))?$");
-	var compatible = false;
-	if (tag.startsWith ("compatible:")) {
-		tag = tag.substringAfter ("compatible:");
-		compatible = true;
-	}
-	var matcher = re.exec (tag);
-	var bump = 1;
-	if (null != matcher[3]) {
-		tag = matcher[1];
-		bump += parseInt (matcher[3]);
-	}
-	if (compatible) {
-	    tag = "compatible:" + tag;
-	}
-	return tag + ":" + bump;
+function bump(tag) {
+    var re = new RegExp("^([^:]+?)(:([0-9]+))?$");
+    var compatible = false;
+    if (tag.startsWith("compatible:")) {
+	tag = tag.substringAfter("compatible:");
+	compatible = true;
+    }
+    var matcher = re.exec(tag);
+    var bump = 1;
+    if (matcher[3] != null) {
+	tag = matcher[1];
+	bump += parseInt(matcher[3]);
+    }
+    if (compatible) {
+	tag = "compatible:" + tag;
+    }
+    return tag + ":" + bump;
 }
